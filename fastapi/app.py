5724bd32771bafd0bd57324f315fdde690cea72f
zach.fry <zach@arthur.ai>
2024-06-12 09:16:08 -0400

    Cluster init

--- fastapi/app.py
+++ fastapi/app.py
@@ -1,9 +1,27 @@
+from contextlib import asynccontextmanager
 from fastapi import FastAPI
 from modules import sentiment_analysis
+import runhouse as rh
 
-app = FastAPI()
+SCORER = None
 
-scorer = sentiment_analysis.SentimentAnalysis()
+
+@asynccontextmanager
+async def lifespan(app):
+    global SCORER
+    cluster = rh.ondemand_cluster(
+        name="fastapi-runhouse-example",
+        instance_type="CPU:2+",
+        provider="aws",
+        region="us-east-2"
+    )
+    cluster.up_if_not()
+    RemoteScorer = rh.module(sentiment_analysis.SentimentAnalysis).to(cluster)
+    SCORER = RemoteScorer()
+    yield
+
+
+app = FastAPI(lifespan=lifespan)
 
 
 @app.get("/ping")
@@ -15,5 +33,5 @@ def ping():
 
 @app.post("/score")
 def score(text):
-    s = scorer.predict(text)
+    s = SCORER.predict(text)
     return s
